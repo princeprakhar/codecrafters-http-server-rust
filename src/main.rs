938c7171ca82187862b56e713d8e5f8ee3246c96
princeprakhar <103184248+princeprakhar@users.noreply.github.com>
2024-08-28 23:43:46 +0530

    pass 2nd  stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::io::Write;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +12,10 @@ fn main() {
      
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok( _stream) => {
+                let mut mut_stream = _stream;
                 println!("accepted new connection");
+                mut_stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("404 OK\r\n\r\n");
             }
             Err(e) => {
                 println!("error: {}", e);
