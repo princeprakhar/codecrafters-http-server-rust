44c81393240fd20e8151a96fb387e96f8b284f5a
princeprakhar <103184248+princeprakhar@users.noreply.github.com>
2024-08-29 11:15:34 +0530

    pass 2nd  stage

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::net::TcpListener;
-use std::{io::Write, net::TcpListener};
+use std::io::Write;
 fn main() {
     println!("Logs from your program will appear here!");
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
             Ok(mut _stream) => {
                 println!("accepted new connection");
                 let response = String::from("HTTP/1.1 200 OK\r\n\r\n");
