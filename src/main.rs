6234eba1e452ce1983e0a8d36cd22e82e0f14894
princeprakhar <prakhardeep173@gmail.com>
2024-09-10 01:00:07 +0530

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+// use core::str;
 #[allow(unused_imports)]
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::{io::BufReader, net::TcpListener};
+
+fn handle_the_connection(mut stream: TcpStream) {
+    // Do something with the stream
+    let buf_reader = BufReader::new(&mut stream);
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let response = match request_line.as_str() {
+        "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
+        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+    };
+    stream.write_all(response.as_bytes()).unwrap();
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
